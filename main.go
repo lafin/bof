03b6eb979b7c8a842ab2dd39b5991e0547405125
Kuvakin Sergey <kuvakin@gmail.com>
2016-03-13 01:30:05 +0300

    :walking:

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
     "io/ioutil"
     "encoding/json"
     "os"
+    "errors"
 )
 
 func parseJson(body []byte) map[string]interface{} {
@@ -35,19 +36,51 @@ func getData(url string) map[string]interface{} {
     return result
 }
 
-func getAccessToken() string {
-    clientId := os.Getenv("CLIENT_ID")
-    clientSecret := os.Getenv("CLIENT_SECRET")
+func getAccessToken(clientId, clientSecret string) string {
     data := getData("https://oauth.vk.com/access_token?client_id="+clientId+"&client_secret="+clientSecret+"&grant_type=client_credentials")
     return data["access_token"].(string)
 }
 
-func getPosts() map[string]interface{} {
-    return getData("https://api.vk.com/method/wall.get?&domain=smcat&count=10&filter=all")
+func getPosts(args ...interface{}) map[string]interface{} {
+    var err error
+    var domain string = ""
+    var count string = "10"
+
+    for i, p := range args {
+        switch i {
+            case 0:
+                val, ok := p.(string)
+                if ok {
+                    domain = val
+                } else {
+                    err = errors.New("Parameter not type string.")
+                }
+            case 1:
+                val, ok := p.(string)
+                if ok {
+                    count = val
+                } else {
+                    err = errors.New("Parameter not type string.")
+                }
+            default:
+                err = errors.New("Wrong parameter count.")
+        }
+    }
+
+    if err != nil {
+        fmt.Println(err)
+        return nil
+    }
+
+    return getData("https://api.vk.com/method/wall.get?&domain="+domain+"&count="+count+"&filter=all")
 }
 
 func main() {
-    accessToken := getAccessToken()
+    clientId := os.Getenv("CLIENT_ID")
+    clientSecret := os.Getenv("CLIENT_SECRET")
+    accessToken := getAccessToken(clientId, clientSecret)
     fmt.Printf("%s\n", accessToken)
-    fmt.Println(getPosts())
+
+    posts := getPosts("smcat")
+    fmt.Println(posts)
 }
