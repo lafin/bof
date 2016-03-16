30408ac30528a5073f8375dfaa74666de3227085
Kuvakin Sergey <kuvakin@gmail.com>
2016-03-17 02:16:21 +0300

    added struct of response :walking:

--- main.go
+++ main.go
@@ -11,6 +11,70 @@ import (
     "regexp"
 )
 
+type Posts struct {
+    Response struct {
+        Count int `json:"count"`
+        Items []struct {
+            Attachments []struct {
+                Link struct {
+                    Caption     string `json:"caption"`
+                    Description string `json:"description"`
+                    IsExternal  int    `json:"is_external"`
+                    Photo       struct {
+                        AlbumID  int    `json:"album_id"`
+                        Date     int    `json:"date"`
+                        Height   int    `json:"height"`
+                        ID       int    `json:"id"`
+                        OwnerID  int    `json:"owner_id"`
+                        Photo130 string `json:"photo_130"`
+                        Photo604 string `json:"photo_604"`
+                        Photo75  string `json:"photo_75"`
+                        Text     string `json:"text"`
+                        Width    int    `json:"width"`
+                    } `json:"photo"`
+                    PreviewPage string `json:"preview_page"`
+                    PreviewURL  string `json:"preview_url"`
+                    Title       string `json:"title"`
+                    URL         string `json:"url"`
+                } `json:"link"`
+                Type string `json:"type"`
+            } `json:"attachments"`
+            Comments struct {
+                CanPost int `json:"can_post"`
+                Count   int `json:"count"`
+            } `json:"comments"`
+            Date   int `json:"date"`
+            FromID int `json:"from_id"`
+            ID     int `json:"id"`
+            Likes  struct {
+                CanLike    int `json:"can_like"`
+                CanPublish int `json:"can_publish"`
+                Count      int `json:"count"`
+                UserLikes  int `json:"user_likes"`
+            } `json:"likes"`
+            OwnerID    int `json:"owner_id"`
+            PostSource struct {
+                Type string `json:"type"`
+            } `json:"post_source"`
+            PostType string `json:"post_type"`
+            Reposts  struct {
+                Count        int `json:"count"`
+                UserReposted int `json:"user_reposted"`
+            } `json:"reposts"`
+            Text string `json:"text"`
+        } `json:"items"`
+    } `json:"response"`
+}
+
+type Repost struct {
+    Response struct {
+        LikesCount   int `json:"likes_count"`
+        PostID       int `json:"post_id"`
+        RepostsCount int `json:"reposts_count"`
+        Success      int `json:"success"`
+    } `json:"response"`
+}
+
 func parseJson(body []byte) map[string]interface{} {
     var data map[string]interface{}
     if err := json.Unmarshal(body, &data); err != nil {
